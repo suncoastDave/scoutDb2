2ed57790cec57231be43f6e56b654d7c9ce72ef4
Dave Smith <github@jarvis725.com>
2023-02-20 15:39:43 -0500

    another test

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Route
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
